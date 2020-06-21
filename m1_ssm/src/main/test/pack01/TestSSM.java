666d910de9aa59bb56096b11a6cc04bdd1e60ca5
hadoop101 <hadoop101@126.com>
2020-06-21 15:28:41 +0800

    SSM MyBastis 与Spring的整合。

--- m1_ssm/src/main/test/pack01/TestSSM.java
+++ m1_ssm/src/main/test/pack01/TestSSM.java
@@ -1,10 +1,36 @@
 package pack01;
 
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
 import org.junit.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import pack01.bean.Account;
+import pack01.dao.AccountDao;
+import pack01.util.MySessionUtil;
 
 public class TestSSM {
     @Test
-    public void test01(){
-        System.out.println();
+    public void test01SpringIOC(){
+        //1:创建容器
+        ApplicationContext applicationContext = new ClassPathXmlApplicationContext("spring.xml");
+        //2:获取dao
+        AccountDao accountDao = (AccountDao) applicationContext.getBean("dao");
+        //3:调用
+        Account account = accountDao.find(1001L);
+        System.out.println(account);
+    }
+
+    @Test
+    public void test02MyBatis(){
+        //1:核心配置文件的数据
+        SqlSessionFactory sqlSessionFactory = (SqlSessionFactory) new ClassPathXmlApplicationContext("spring.xml").getBean("sqlSessionFactory");
+        SqlSession session =sqlSessionFactory.openSession();
+        //2:再考虑测试映射文件
+        System.out.println(session);
+        AccountDao accountDao = session.getMapper(AccountDao.class);
+        //3:增删改查
+        Account account = accountDao.find(1001L);
+        System.out.println(account);
     }
 }
